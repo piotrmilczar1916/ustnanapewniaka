d3a45828624dfa22a6cb23b73efd389f81b22e9d
Piotr Milczarski <s35888@pjwstk.edu.pl>
2026-08-02 20:13:35 +0200

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { FAQ } from "@/components/home/FAQ";
-import { Features } from "@/components/home/Features";
 import { Hero } from "@/components/home/Hero";
 import { HowItWorks } from "@/components/home/HowItWorks";
 import { KomisjaAI } from "@/components/home/KomisjaAI";
@@ -11,7 +10,6 @@ export default function HomePage() {
       <Hero />
       <HowItWorks />
       <KomisjaAI />
-      <Features />
       <Pricing />
       <FAQ />
     </>
